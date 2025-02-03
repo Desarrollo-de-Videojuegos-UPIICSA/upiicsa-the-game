aa5b0bd115a3aa67d1d048b6707655cd6155860a
El_Colibri_zurdo <angeltorralva@hotmail.com>
2025-02-02 21:11:36 -0600

    Flecha guia

---
+++ Assets/Scripts/DireccionarFlecha.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DireccionarFlecha : MonoBehaviour
+{
+    public Transform jugador;
+    public Transform objetivo;
+
+     public float fixedYRotation = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direccion = (objetivo.position - transform.position).normalized;
+
+        transform.up = direccion;
+        
+        
+    }
+
+
+    void LateUpdate() {
+        
+        Vector3 jugadorPos = jugador.position;
+        Vector3 posicionNueva = new Vector3(jugadorPos.x, jugadorPos.y + 4.5f, jugadorPos.z);
+        transform.position = posicionNueva;
+    }
+}
